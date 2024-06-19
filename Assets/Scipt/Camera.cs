87fdf725b7a98b4a9d494e8d2b58f2470d3d7950
Zedra19 <zedra1911@gmail.com>
2024-06-19 12:21:00 +0700

    finish build (masih ada yg kurang)
    tinggal destroy mob on hit, game over camera, sfx, sama animasi nembak player

---
+++ Assets/Scipt/Camera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    Vector3 StartPos;
+    Vector3 gameoverPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

8fca5c40888f896824c4bc6dfadccea7302753e7
Zedra19 <zedra1911@gmail.com>
2024-06-19 10:40:55 +0700

    animasi gerak (belum sempurna)

--- Assets/Scipt/Player.cs
+++ Assets/Scipt/Player.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Animations;
 public class Player : MonoBehaviour
 {
+    Animator anim;
+    CharacterController characterController;
+    [SerializeField] private float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement();
+    }
+
+    void movement(){
+        if(Input.GetKey(KeyCode.A)){
+            characterController.Move(Vector3.left * speed * Time.deltaTime);
+            anim.SetBool("Left",true);
+            anim.SetBool("Right",false);
+        }else if(Input.GetKey(KeyCode.D)){
+            characterController.Move(Vector3.right * speed * Time.deltaTime);
+            anim.SetBool("Left",false);
+            anim.SetBool("Right",true);
+        }
+    }
+
+    void shoot(){
+
     }
 }

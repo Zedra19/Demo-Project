a8fe8e3a5faf8afe3db0a81d957736ca7924a0d2
Zedra19 <zedra1911@gmail.com>
2024-06-19 09:55:31 +0700

    main menu fix + sound

--- Assets/Scipt/MainMenu/AudioManager.cs
+++ Assets/Scipt/MainMenu/AudioManager.cs
@@ -2,13 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager Instance;
+    public AudioSource MainMenuSource;
+    public AudioSource GameplaySource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            GameplaySource.Stop();
+            MainMenuSource.Play();
+        }else if(SceneManager.GetActiveScene().buildIndex == 1){
+            MainMenuSource.Stop();
+            GameplaySource.Play();
+        }
     }
 
     // Update is called once per frame
